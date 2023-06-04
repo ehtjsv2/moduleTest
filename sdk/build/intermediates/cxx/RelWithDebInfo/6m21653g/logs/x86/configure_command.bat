393341039dcca5853b6053aa3a45d6ba27362fe7
ehtjsv2 <ehtjsv2@naver.com>
2023-06-05 00:37:42 +0900

    1.0.3 test3

--- sdk/build/intermediates/cxx/RelWithDebInfo/6m21653g/logs/x86/configure_command.bat
+++ sdk/build/intermediates/cxx/RelWithDebInfo/6m21653g/logs/x86/configure_command.bat
@@ -1,6 +1,6 @@
 @echo off
 "C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
-  "-HC:\\Users\\user\\Desktop\\AndroidAppProgramming\\-KNUCapstoneDesignProject2\\TestApp\\sdk\\libcxx_helper" ^
+  "-HC:\\Users\\user\\AndroidStudioProjects\\moduleTest\\sdk\\libcxx_helper" ^
   "-DCMAKE_SYSTEM_NAME=Android" ^
   "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
   "-DCMAKE_SYSTEM_VERSION=21" ^
@@ -11,9 +11,9 @@
   "-DCMAKE_ANDROID_NDK=C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
   "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
   "-DCMAKE_MAKE_PROGRAM=C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\user\\Desktop\\AndroidAppProgramming\\-KNUCapstoneDesignProject2\\TestApp\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\6m21653g\\obj\\x86" ^
-  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\user\\Desktop\\AndroidAppProgramming\\-KNUCapstoneDesignProject2\\TestApp\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\6m21653g\\obj\\x86" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\user\\AndroidStudioProjects\\moduleTest\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\6m21653g\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\user\\AndroidStudioProjects\\moduleTest\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\6m21653g\\obj\\x86" ^
   "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
-  "-BC:\\Users\\user\\Desktop\\AndroidAppProgramming\\-KNUCapstoneDesignProject2\\TestApp\\sdk\\.cxx\\RelWithDebInfo\\6m21653g\\x86" ^
+  "-BC:\\Users\\user\\AndroidStudioProjects\\moduleTest\\sdk\\.cxx\\RelWithDebInfo\\6m21653g\\x86" ^
   -GNinja ^
   "-DANDROID_STL=c++_shared"
